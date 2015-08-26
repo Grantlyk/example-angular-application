21bb429a07d0279145354707082409c9fd4186a0
Grant Leadbetter <grant@nespite.io>
2015-08-26 14:11:17 +0100

    added People factory

--- js/app.js
+++ js/app.js
@@ -1,14 +1,38 @@
 angular.module('angularExample', [])
 	
-.controller('mainController', function(){
+.controller('mainController', function(People){
 	var vm = this;
 
 	vm.message = "Two way databinding!";
 
-	vm.people = [
-		{ name: "James", age: "21" },
-		{ name: "David", age: "25" },
-		{ name: "Andrew", age: "19" }
-	];
+	vm.personData = {};
 
+	vm.people = People.list();
+
+	// vm.people = [
+	// 	{ name: "James", age: "21" },
+	// 	{ name: "David", age: "25" },
+	// 	{ name: "Andrew", age: "19" }
+	// ];
+
+	vm.addPerson = function(){
+		People.addPerson(vm.personData);
+		vm.personData = {};
+	};
+
+})
+
+.factory('People', function(){
+	var people = [];
+	return {
+		list: function(){
+			return people;
+		},
+		addPerson: function(personData){
+			people.push({
+				name: personData.name,
+				age: personData.age
+			});
+		}
+	};
 });
